54ae9e0b97112fc6605418eafad9858e0e46f715
Robert <r.hansana45@gmail.com>
2022-01-10 13:08:31 +0100

    marche sur tel

--- app/src/main/java/com/example/lesign/MainActivity.java
+++ app/src/main/java/com/example/lesign/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.lesign;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView map = findViewById(R.id.map);
+        WebView map = (WebView) findViewById(R.id.map);
 
+        map.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("MyApplication", consoleMessage.message() + " -- From line " +
+                        consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                return true;
+            }
+
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        map.getSettings().setDomStorageEnabled(true);
         map.getSettings().setJavaScriptEnabled(true);
-        map.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        map.getSettings().setAppCacheEnabled(true);
+        map.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        map.getSettings().setGeolocationEnabled(true);
+        map.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getPath());
+        map.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
 
         map.loadUrl("file:///android_asset/html/map.html");
     }
